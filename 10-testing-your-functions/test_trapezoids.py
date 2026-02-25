21d446f381c17dc0e5b612a2ea7cdb6a5a386d5b
Dominic Bishop <dombish@iu.edu>
2026-02-25 14:03:56 -0500

    worked through examples from lecture on 2/25/26

---
+++ 10-testing-your-functions/test_trapezoids.py
@@ -0,0 +1,20 @@
+from trapezoids import trapezint
+
+def hat(x):
+    """Simple function that equals x between [0,1], 2-x between [1,2], and 0 otherwise"""
+    if 0 <= x <= 1:
+        return x
+    elif 1 < x <= 2:
+        return 2 - x
+    return 0
+
+def test_trapezint1_hat04():
+    """Can we integrate a step function from 0 to 1 with one trapezoid?"""
+    result = trapezint(hat, 0, 4)
+    assert result == 1
+
+def test_trapezint_sin0pi():
+    import numpy as np
+    
+    result = trapezint(np.sin, 0, np.pi, n=2)
+    assert result == abs(result - np.pi/2) < 1e-6
